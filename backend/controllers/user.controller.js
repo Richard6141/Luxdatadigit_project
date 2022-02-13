d7ced3b95be05dc8d12ac3ac420a18be3224de84
richard <somasse-richard.salanon@epitech.eu>
2022-02-13 03:59:08 +0100

    Add function userupdate to userController

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,8 +2,7 @@ const models = require("../models");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Validator = require("fastest-validator");
-const User = require('../models').Task;
-
+const User = require("../models").Task;
 
 function singUp(req, res) {
   models.User.findOne({ where: { email: req.body.email } })
@@ -70,50 +69,64 @@ function singUp(req, res) {
     });
 }
 
-function logIn(req, res){
-    models.User.findOne({where:{email: req.body.email}}).then(user =>{
-        if(user === null){
-            res.status(401).json({
-                message: "Bad credentilas !"
-            })
-        }else{
-            bcryptjs.compare(req.body.password, user.password, function(err, result){
-                if(result){
-                    const token = jwt.sign({
-                        email: user.email,
-                        userId: user.id,
-                    }, process.env.JWT_KEY, function(err, token){
-                        res.status(200).json({
-                            message: "Authentificate successful !",
-                            token:token,
-                            decoc : jwt.decode(token).userId 
-                        })
-                    })
-                }else{
-                    res.status(401).json({
-                        message: "Bad credentilas !"
-                    })
-                        }
-            })
-        } 
-    }).catch(error =>{
-        res.status(500).json({
-            message: "Something went wrong !"
-        })
+function logIn(req, res) {
+  models.User.findOne({ where: { email: req.body.email } })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({
+          message: "Bad credentilas !",
+        });
+      } else {
+        bcryptjs.compare(
+          req.body.password,
+          user.password,
+          function (err, result) {
+            if (result) {
+              const token = jwt.sign(
+                {
+                  email: user.email,
+                  userId: user.id,
+                },
+                process.env.JWT_KEY,
+                function (err, token) {
+                  res.status(200).json({
+                    message: "Authentificate successful !",
+                    token: token,
+                    decoc: jwt.decode(token).userId,
+                  });
+                }
+              );
+            } else {
+              res.status(401).json({
+                message: "Bad credentilas !",
+              });
+            }
+          }
+        );
+      }
     })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong !",
+      });
+    });
 }
 
 function allusers(req, res) {
-  models.User.findAll({include:[{
-    model: models.Task,
-  }]})
+  models.User.findAll({
+    include: [
+      {
+        model: models.Task,
+      },
+    ],
+  })
     .then((result) => {
       res.status(200).json(result);
     })
     .catch((error) => {
       res.status(500).json({
         message: "Something went wrong",
-        error: error
+        error: error,
       });
     });
 }
@@ -153,7 +166,7 @@ function destroy(req, res) {
   const id = req.params.id;
   // const userId = req.user.userId;
 
-  models.User.destroy({ where: { id: id} })
+  models.User.destroy({ where: { id: id } })
     .then((result) => {
       if (result) {
         res.status(200).json({
@@ -175,67 +188,93 @@ function destroy(req, res) {
 
 function userupdate(req, res) {
   const id = req.params.id;
-  const is_exist = models.User.find(c.id ===parseInt(id));
-  if(!is_exist) res.status(404).send("The user with this id deoesn't exist !")
-
-    bcryptjs.genSalt(10, function (err, salt) {
-      bcryptjs.hash(req.body.password, salt, function (err, hash) {
-        const usertoupdate = {
-          name: req.body.name,
-          surname: req.body.surname,
-          email: req.body.email,
-          password: hash,
-          nationality: req.body.nationality,
-          birthday: req.body.birthday,
-          speciality: req.body.speciality,
-        };
-
-        const schema = {
-          name: { type: "string", optional: false, max: "50" },
-          surname: { type: "string", optional: false, max: "50" },
-          email: { type: "string", optional: false },
-          password: { type: "string", optional: false },
-          nationality: { type: "string", optional: false, max: "50" },
-          birthday: { type: "string", optional: false },
-          speciality: { type: "string", optional: false, max: "50" },
-        };
-        const v = new Validator();
-        const validationResponse = v.validate(usertoupdate, schema);
-
-        if (validationResponse !== true) {
-          return res.status(400).json({
-            message: "Validation failed !",
-            errors: validationResponse,
-          });
-        }
-
-        models.User.create(usertoupdate)
-          .then((result) => {
-            res.status(201).json({
-              message: "User created successfully",
-              User: result,
-            });
-          })
-          .catch((error) => {
-            res.status(500).json({
-              message: "Something went wrong !",
-              error: error,
-            });
-          });
-      });
-    });
+  const userId = req.user.userId;
+  const is_exist = models.User.findByPk(id);
+  if (!is_exist) {
+    res.status(404).send("The user with this id deoesn't exist !");
   }
 
+  bcryptjs.genSalt(10, function (err, salt) {
+    bcryptjs.hash(req.body.password, salt, function (err, hash) {
+      const usertoupdate = {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: hash,
+        nationality: req.body.nationality,
+        birthday: req.body.birthday,
+        speciality: req.body.speciality,
+      };
 
+      const schema = {
+        name: { type: "string", optional: false, max: "50" },
+        surname: { type: "string", optional: false, max: "50" },
+        email: { type: "string", optional: false },
+        password: { type: "string", optional: false },
+        nationality: { type: "string", optional: false, max: "50" },
+        birthday: { type: "string", optional: false },
+        speciality: { type: "string", optional: false, max: "50" },
+      };
+      const v = new Validator();
+      const validationResponse = v.validate(usertoupdate, schema);
 
+      if (validationResponse !== true) {
+        return res.status(400).json({
+          message: "Validation failed !",
+          errors: validationResponse,
+        });
+      }
+
+      // usertoupdate.forEach((tag) => {
+      //   if (is_exist) {
+      //     tag.name === usertoupdate.name ? usertoupdate.name : tag.name;
+      //     tag.surname === usertoupdate.surname
+      //       ? usertoupdate.surname
+      //       : tag.surname;
+      //     tag.password === usertoupdate.password
+      //       ? usertoupdate.password
+      //       : tag.password;
+      //     tag.nationality === usertoupdate.nationality
+      //       ? usertoupdate.nationality
+      //       : tag.nationality;
+      //     tag.birthday === usertoupdate.birthday
+      //       ? usertoupdate.birthday
+      //       : tag.birthday;
+      //     tag.speciality === usertoupdate.speciality
+      //       ? usertoupdate.speciality
+      //       : tag.speciality;
+      //   }
+      // });
+
+      models.User.update(usertoupdate, { where: { id: id, } })
+        .then((result) => {
+          if (result) {
+            res.status(200).json({
+              message: "User updated successfully !",
+              User: usertoupdate,
+            });
+          } else {
+            res.status(404).json({
+              message: "User with this id doesn't found",
+            });
+          }
+        })
+        .catch((error) => {
+          res.status(200).json({
+            message: "Something went wrong !",
+            Task: error,
+          });
+        });
+    });
+  });
+}
 
 module.exports = {
   singUp: singUp,
-  logIn:logIn,
-  allusers:allusers,
-  show:show,
-  // logout:logout, 
+  logIn: logIn,
+  allusers: allusers,
+  show: show,
+  // logout:logout,
   destroy: destroy,
-  userupdate: userupdate
-
+  userupdate: userupdate,
 };
